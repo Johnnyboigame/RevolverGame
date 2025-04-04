665eb461256fe4f017f83a383f320da768e2b99b
Johnnyboigame <jcovert215@gmail.com>
2025-04-04 16:25:31 -0500

    t

--- game.js
+++ game.js
@@ -24,21 +24,23 @@ function animate() {
 
 
     for (const element of playerArrary) {
-        element.update();
+        element.draw();
     }
+
+
 }
 
 animate();
-/*
+
 
 //keypressed event listener for input
-window.addEventListener('keydown', this.check, false);
+window.addEventListener('keydown', checkKeyup(), false);
 
-function check(e) {
+function checkKeyup() {
     var code = e.keycode;
     switch(code) {
-        case 76: alert("l"); break;
+        case 76: playerArrary[0].update(); break;
+        case 74: playerArrary[1].update(); break;
         default: alert(code); break;
     }
 }
-    */
